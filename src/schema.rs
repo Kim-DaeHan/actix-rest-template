9b03b60f61ae3d50aa5fc7eaacf8d4ed8d404344
Kim-DaeHan <Kim-DaeHan@users.noreply.github.com>
2023-12-29 17:20:03 +0900

    .

--- src/schema.rs
+++ src/schema.rs
@@ -1,13 +1,6 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    newtable (id) {
-        id -> Varchar,
-        post_title -> Nullable<Varchar>,
-    }
-}
-
-diesel::table! {
     posts (id) {
         #[max_length = 255]
         id -> Varchar,
@@ -19,8 +12,3 @@ diesel::table! {
         updated_at -> Nullable<Timestamp>,
     }
 }
-
-diesel::allow_tables_to_appear_in_same_query!(
-    newtable,
-    posts,
-);
